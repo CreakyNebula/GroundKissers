7665c7d738607e6193c1ceaaae88b0bcb1d85bd7
Aitor <alechagomiron@gmail.com>
2024-10-09 20:04:09 +0200

    add lateral spears

--- GroundKissers/Assets/_Scripts/Aitor/Managers/Obstacles/LateralSpearsManager.cs
+++ GroundKissers/Assets/_Scripts/Aitor/Managers/Obstacles/LateralSpearsManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LateralSpearsManager : MonoBehaviour
 {
+    private Animator anim;
+    
+    [SerializeField] private float minTimeBetweenActivations = 3f;
+    [SerializeField] private float maxTimeBetweenActivations = 7f;
+    [SerializeField] private float actionDuration = 0.1f;
+    [SerializeField] private float cooldownTime = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(ActivateActionPeriodically());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ActivateActionPeriodically()
     {
-        
+        while (true)
+        {
+            // Generar un tiempo aleatorio entre activaciones
+            float randomTimeBetweenActivations = Random.Range(minTimeBetweenActivations, maxTimeBetweenActivations);
+            
+            yield return new WaitForSeconds(randomTimeBetweenActivations);
+
+            anim.SetBool("Action", true);
+
+            yield return new WaitForSeconds(actionDuration);
+
+            anim.SetBool("Action", false);
+
+            yield return new WaitForSeconds(cooldownTime);
+        }
     }
 }
