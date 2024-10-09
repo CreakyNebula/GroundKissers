0cf5b3d42bfbc4c9f725fe1add769fb4bef438ea
barbituricos <morganrfdez17@gmail.com>
2024-10-09 22:37:49 +0200

    add EndGameMenu

---
+++ GroundKissers/Assets/_Scripts/Morgan/EndGame.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] GameObject EndGamePanel;
+
+    private int muerte;
+
+    public void MostrarEndGame()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void MainMenu()
+    {
+        //SceneManager.LoadScene("MainMenu");
+        UnityEditor.EditorApplication.isPlaying = false; // Para detener el juego en el editor
+    }
+
+    public void Muerte()
+    {
+        muerte++;
+    }
+}
--- GroundKissers/Assets/_Scripts/Morgan/Timer.cs
+++ GroundKissers/Assets/_Scripts/Morgan/Timer.cs
@@ -6,32 +6,33 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] int min, seg;
-    [SerializeField] TMP_Text timer;
+    [SerializeField] int min, seg;     //variable de min y segs
+    [SerializeField] TMP_Text timer;   //componente de texto se encarga de mostrarlo por pantalla
 
-    private float remaining;
-    private bool onGoing;
+    private float remaining;    //almacena el tiempo total que queda
+    private bool onGoing;       
 
-    private void Awake()
+    private void Awake()       //metodo que se llama automaticamente cuando el script comienza
     {
-        remaining = (min * 60) + seg;
-        onGoing= true;
+        remaining = (min * 60) + seg;    //convertimos min en seg y sumamos los seg para el tiempo total
+        onGoing= true;      //indica que el temporizador está activo
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (onGoing)
+        if (onGoing)      //comprueba si el temporizador está en marcha
         {
-            remaining -= Time.deltaTime;
-            if(remaining < 1)
+            remaining -= Time.deltaTime;      //se resta el tiempo que ha pasado en ese frae del tiempo restante
+
+            if(remaining < 1)     //si el tiempo es menor a 1 detenemos el contador
             {
-                onGoing= false;
+                onGoing= false;    
                 //Acabar Partida
             }
-            int tempMin = Mathf.FloorToInt(remaining / 60);
-            int tempSeg = Mathf.FloorToInt(remaining % 60);
-            timer.text = string.Format("{0:00}:{1:00}", tempMin, tempSeg);
+            int tempMin = Mathf.FloorToInt(remaining / 60);     //calcula cuantos minutos quedan dvidiendo entre 60
+            int tempSeg = Mathf.FloorToInt(remaining % 60);     //clacula segundos restantes 
+            timer.text = string.Format("{0:00}:{1:00}", tempMin, tempSeg);    //convierte los mintos y segundos en texto y lo actualiza en la pantalla
         }
     }
 }
